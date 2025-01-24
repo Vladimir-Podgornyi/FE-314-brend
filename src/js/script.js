edcd92ecc5ba74d43910625d9bdfab7362c3c923
Vladimir Podgornyi <vl.podgornyi@gmail.com>
2025-01-24 16:13:14 +0100

    das schicke ich erste mal

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ let swiper = new Swiper(".swiper", {
     });
 
 
-    
+  
     
     
     /* Читать Далее ------------------------- */
@@ -16,7 +16,7 @@ let swiper = new Swiper(".swiper", {
         const brandMore = document.querySelectorAll('.brand-more'); // Скрытые элементы
         const leaseMore = document.querySelector('.lease__more'); // Кнопка "Показать всё"
         const leaseClouse = document.querySelector('.lease__clouse'); // Кнопка "Скрыть"
-        const brandTabl = document.querySelectorAll('.brand-tbl')
+        const brandTabl = document.querySelectorAll('.brand-js')
     
         // Кнопка "Показать всё"
         leaseMore.addEventListener('click', () => {
@@ -26,18 +26,7 @@ let swiper = new Swiper(".swiper", {
           leaseMore.style.display = 'none'; // Скрываем кнопку "Показать всё"
           leaseClouse.style.display = 'flex'; // Показываем кнопку "Скрыть"
         });
-        // Кнопка "Показать всё" в планшете
-        leaseMore.addEventListener('click', () => {
-          brandTabl.forEach((item) => {
-            item.style.display = 'flex'; // Показываем элементы
-          });
-          brandTabl.style.display = 'none'; // Скрываем кнопку "Показать всё"
-          leaseClouse.style.display = 'flex'; // Показываем кнопку "Скрыть"
-        });
-    
-
-
-
+  
         // Кнопка "Скрыть"
         leaseClouse.addEventListener('click', () => {
           brandMore.forEach((item) => {
@@ -47,16 +36,49 @@ let swiper = new Swiper(".swiper", {
           leaseMore.style.display = 'flex'; // Показываем кнопку "Показать всё"
         });
 
-          // Кнопка "Скрыть" в планшете
-          leaseClouse.addEventListener('click', () => {
-            brandTabl.forEach((item) => {
-              item.style.display = 'none'; // Скрываем элементы
-            });
-            leaseClouse.style.display = 'none'; // Скрываем кнопку "Скрыть"
-            brandTabl.style.display = 'flex'; // Показываем кнопку "Показать всё"
-          });
+         
       }
-    
       readMore();
+
+
+
+      function screenMedia() {
+        const mediaSize = window.matchMedia("(min-width: 500px) and (max-width: 768px)");
+        
+        if (mediaSize.matches) {
+            readMoreTbl () ; // Вызываем функцию, если условие выполняется
+        }
+    }
+
+    function readMoreTbl () {
+
+        
+        const leaseMore = document.querySelector('.lease__more'); // Кнопка "Показать всё"
+        const leaseClouse = document.querySelector('.lease__clouse'); // Кнопка "Скрыть"
+        const brandTabl = document.querySelectorAll('.brand-js')
+
+
+      leaseMore.addEventListener('click', () => {
+        brandTabl.forEach((item) => {
+          item.style.display = 'flex'; // Показываем элементы
+        });
+      });
     
+
+
+       // Кнопка "Скрыть" в планшете
+       leaseClouse.addEventListener('click', () => {
+        brandTabl.forEach((item) => {
+          item.style.display = 'none'; // Скрываем элементы
+        });
+      });
+    }
+
+
+
+  // Добавляем обработчик события для динамической проверки при изменении размера экрана
+window.addEventListener("resize", screenMedia);
+
+// Выполняем проверку при загрузке страницы
+screenMedia();
     
