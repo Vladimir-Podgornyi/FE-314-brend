dc73939eea1371978d8e34d70533789de965bb99
Vladimir Podgornyi <vl.podgornyi@gmail.com>
2025-01-24 16:19:35 +0100

    vertig

--- src/js/script.js
+++ src/js/script.js
@@ -14,26 +14,26 @@ let swiper = new Swiper(".swiper", {
     
       function readMore() {
         const brandMore = document.querySelectorAll('.brand-more'); // Скрытые элементы
-        const leaseMore = document.querySelector('.lease__more'); // Кнопка "Показать всё"
-        const leaseClouse = document.querySelector('.lease__clouse'); // Кнопка "Скрыть"
+        const leaseMore = document.querySelector('.lease__more'); // Кнопка Показать всё
+        const leaseClouse = document.querySelector('.lease__clouse'); // Кнопка Скрыть
         const brandTabl = document.querySelectorAll('.brand-js')
     
-        // Кнопка "Показать всё"
+        // Кнопка Показать всё плюс показать элементы
         leaseMore.addEventListener('click', () => {
           brandMore.forEach((item) => {
-            item.style.display = 'flex'; // Показываем элементы
+            item.style.display = 'flex'; // Тут показываем элементы
           });
-          leaseMore.style.display = 'none'; // Скрываем кнопку "Показать всё"
-          leaseClouse.style.display = 'flex'; // Показываем кнопку "Скрыть"
+          leaseMore.style.display = 'none'; // Тут скрываем кнопку Показать всё
+          leaseClouse.style.display = 'flex'; // Тут показываем кнопку Скрыть
         });
   
-        // Кнопка "Скрыть"
+        // Кнопка Скрыть
         leaseClouse.addEventListener('click', () => {
           brandMore.forEach((item) => {
-            item.style.display = 'none'; // Скрываем элементы
+            item.style.display = 'none'; // Скрываем лишнии три элемента
           });
-          leaseClouse.style.display = 'none'; // Скрываем кнопку "Скрыть"
-          leaseMore.style.display = 'flex'; // Показываем кнопку "Показать всё"
+          leaseClouse.style.display = 'none'; // Скрываем кнопку Скрыть
+          leaseMore.style.display = 'flex'; // Показываем кнопку Показать всё
         });
 
          
@@ -41,23 +41,23 @@ let swiper = new Swiper(".swiper", {
       readMore();
 
 
-
+        // тут прописываем скрипт что бы была проверка ширины экрана
       function screenMedia() {
         const mediaSize = window.matchMedia("(min-width: 500px) and (max-width: 768px)");
         
         if (mediaSize.matches) {
-            readMoreTbl () ; // Вызываем функцию, если условие выполняется
+            readMoreTbl () ; // если условие выполняется тогда работает следующая функция ниже 
         }
     }
 
+    // функция для кнопок которые в планшете 
     function readMoreTbl () {
 
-        
-        const leaseMore = document.querySelector('.lease__more'); // Кнопка "Показать всё"
-        const leaseClouse = document.querySelector('.lease__clouse'); // Кнопка "Скрыть"
-        const brandTabl = document.querySelectorAll('.brand-js')
+        const leaseMore = document.querySelector('.lease__more'); // Кнопка Показать всё
+        const leaseClouse = document.querySelector('.lease__clouse'); // Кнопка Скрыть
+        const brandTabl = document.querySelectorAll('.brand-js') // Два блока плланшета 
 
-
+      // показать в планшете 
       leaseMore.addEventListener('click', () => {
         brandTabl.forEach((item) => {
           item.style.display = 'flex'; // Показываем элементы
@@ -66,7 +66,7 @@ let swiper = new Swiper(".swiper", {
     
 
 
-       // Кнопка "Скрыть" в планшете
+       // Скрыть в планшете
        leaseClouse.addEventListener('click', () => {
         brandTabl.forEach((item) => {
           item.style.display = 'none'; // Скрываем элементы
@@ -76,7 +76,7 @@ let swiper = new Swiper(".swiper", {
 
 
 
-  // Добавляем обработчик события для динамической проверки при изменении размера экрана
+  // Добавляем  события для динамической проверки при изменении размера экрана
 window.addEventListener("resize", screenMedia);
 
 // Выполняем проверку при загрузке страницы
